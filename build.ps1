deb289136eba568a4c60767e7c6d2dbf5e8b4dbe
Kohányi Róbert <kohanyi.robert@gmail.com>
2020-02-27 09:47:09 +0100

    Update build.ps1, create TopTal/Upwork resume TeX

--- build.ps1
+++ build.ps1
@@ -1,9 +1,13 @@
+param (
+  [string]$texfile = "main.tex"
+)
 $image = "blang/latex:ctanfull"
 & docker run `
     --rm `
     --interactive `
+    --tty `
     --user=${id -u}:${id -g} `
     --net=none `
     --volume ${pwd}:/data `
     ${image} `
-    latexmk -cd -pvc -f -interaction=batchmode -pdflua -view=none main.tex
+    latexmk -cd -pvc -f -interaction=batchmode -pdflua -view=none $texfile
