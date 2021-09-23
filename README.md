59cfa9833943f053b8dedd7556ce4f0b674028d9
Kohányi Róbert <kohanyi.robert@gmail.com>
2021-09-23 14:34:51 +0200

    Update main.tex, build.ps1 and add README

---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+
+- Edit `main.tex`
+- Run `.\build.ps1`
+- Open `main.pdf`
+
+Alternatively create `other.tex`, run `.\build.ps1 -texfile other.tex` and open `other.pdf`.
--- build.ps1
+++ build.ps1
@@ -1,6 +1,19 @@
 param (
   [string]$texfile = "main.tex"
 )
+
+$source = (PWD).Path
+if ($env:DOCKER_TOOLBOX_PATH)
+{
+  $uri = [System.Uri]$source
+  $drive = $uri.Segments[1].TrimEnd(':/').ToLower()
+  $parts = $uri.Segments[2..($uri.Segments.Length - 1)]
+  $arr = @()
+  $arr += $drive + '/'
+  $arr += $parts
+  $source = '/' + ($arr -Join '')
+}
+
 $image = "blang/latex:ctanfull"
 & docker run `
     --rm `
@@ -8,6 +21,6 @@ $image = "blang/latex:ctanfull"
     --tty `
     --user=${id -u}:${id -g} `
     --net=none `
-    --volume ${pwd}:/data `
+    --mount type=bind,source="${source}",target=/data `
     ${image} `
     latexmk -cd -pvc -f -interaction=batchmode -pdflua -view=none $texfile
